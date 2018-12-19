284035972cdc4114b32400c6b511e0c8b4836b92
Dongpyeong Seo <bludrimjr@gmail.com>
2018-12-20 00:25:55 +0900

    update

--- noob_scrapper/app.py
+++ noob_scrapper/app.py
@@ -1,8 +1,30 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, session
+from werkzeug.security import generate_password_hash
+import string
+import os
 import requests as r
 import argparse
 
 app = Flask(__name__)
+
+def random_string_generator(length):
+    return ''.join([random.choice(string.ascii_letters + string.digits) for n in range(length)])
+
+def save_response(res):
+    '''
+    try:
+        tempfile.tempdir = "./scraps"
+        temp = tempfile.NamedTemporaryFile(prefix="scrap_")
+        temp.write(res.text)
+        return temp
+    except:
+        temp.close()
+        return None
+    '''
+    fname = random_string_generator(32)
+    f = open("scrap_" + fname, 'w')
+    f.write(res.text)
+    f.close()
 
 @app.route("/")
 def index():
@@ -17,26 +39,38 @@ def scrap():
         # print(f"request.url : {request.url} / request.path : {request.path}")
         # request.url : http://143.248.2.130:8080/scrap / request.path : /scrap
 
+        pages = []
         if url != "":
             if not (url.startswith("http://") or url.startswith("https://")):
                 url = "http://" + url
 
             try:
                 res = r.get(url)
-                print(f"Response of {url} : ")
-                print(res.text)
+                #print(f"Response of {url} : ")
+                #print(res.text)
+                pages.append({'url' : url})
             except:
                 return "Not a good url... hmm..."
 
-            #return render_template("show.html")
-        return f"Your url : '{url}'"
+        return render_template("show.html", pages=pages)
+
+@app.route("/scraps/<uid>/<path:subpath>")
+def view(uid, subpath):
+    print(f"uid : {uid} / subpath : {subpath}")
+    return "Not Implemented"
 
 @app.route("/login", methods = ["GET", "POST"])
 def login():
     if request.method == "GET":
         return render_template("login.html")
     elif request.method == "POST":
-        return "login not yet implemented.. Sorry!"
+        uid = request.form['id']
+        upw = request.form['pw']
+        if uid == "" or upw == "":
+            return "Not a valid id / pw"
+        
+        pwhash = generate_password_hash(upw)
+        return f"Your id : {uid} / Your hash(pw) : {pwhash}"
     else:
         return "Wrong request method"
 
@@ -48,7 +82,7 @@ def argcheck():
 
 if __name__ == "__main__":
     args = argcheck()
-    port = 8080
+    port = 30039
     if args.p != None and args.p > 0 and args.p < 65536:
         port = args.p
     app.run(debug=True,host='0.0.0.0', port=port)
