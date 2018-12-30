567c20caa0c898f7bf64c1589945fd0410da7c4c
Dongpyeong Seo <bludrimjr@gmail.com>
2018-12-31 00:12:03 +0900

    Finished basic functionality

--- noob_scrapper/app.py
+++ noob_scrapper/app.py
@@ -1,148 +1,158 @@
-from flask import Flask, render_template, request, url_for, session, flash, redirect
+#!/usr/bin/env python3
+from flask import Flask, render_template, request, url_for, session, flash, redirect, make_response
 from werkzeug.security import generate_password_hash, check_password_hash
-import os, string
+import os
 import requests as r
-import argparse
 
-
+from misc import argcheck, random_string_generator
 from config import secret_key
-from database import init_db
-from database import db_session
-from models import User
+from database import init_db, db_session
+from models import User, Scrap
 
 app = Flask(__name__)
 app.secret_key = secret_key
 
-def random_string_generator(length):
-    return ''.join([random.choice(string.ascii_letters + string.digits) for n in range(length)])
-
-def save_response(res):
-    '''
-    try:
-        tempfile.tempdir = "./scraps"
-        temp = tempfile.NamedTemporaryFile(prefix="scrap_")
-        temp.write(res.text)
-        return temp
-    except:
-        temp.close()
-        return None
-    '''
-    fname = random_string_generator(32)
-    f = open("scrap_" + fname, 'w')
-    f.write(res.text)
-    f.close()
+scraps_dir = "templates/scraps"
 
 def get_user(uid, upw=""):
     if upw == "":
         return db_session.query(User).filter(User.id==uid).all()
     else:
         users = db_session.query(User).filter(User.id==uid).all()
-        for user in users:
-            if check_password_hash(user.pw, upw):
-                return [user]
-        return []
+        return [user for user in users if check_password_hash(user.pw, upw)]
 
 @app.route("/")
 def index():
     return render_template("main.html")
 
-@app.route("/scrap", methods=["GET", "POST"])
+@app.route("/scrap", methods=["POST"])
 def scrap():
-    if request.method == "GET":
-        return "Hi! What do you want to get?"
-    elif request.method == "POST":
-        url = request.form["url"] if "url" in request.form else ""
-        # print(f"request.url : {request.url} / request.path : {request.path}")
-        # request.url : http://143.248.2.130:8080/scrap / request.path : /scrap
+    if not "id" in session:
+        flash("You've not logged in")
+        return redirect("/")
 
-        pages = []
-        if url != "":
-            if not (url.startswith("http://") or url.startswith("https://")):
-                url = "http://" + url
+    url = request.form.get("url", "")
+    title = request.form.get("title", "No title")
 
-            try:
-                res = r.get(url)
-                #print(f"Response of {url} : ")
-                #print(res.text)
-                pages.append({'url' : url})
-            except:
-                return "Not a good url... hmm..."
+    if url == "":
+        flash("Not a good url... hmm...")
+        return redirect(url_for("index"))
 
-        return render_template("show.html", pages=pages)
+    if not (url.startswith("http://") or url.startswith("https://")):
+        url = "http://" + url
 
-@app.route("/scraps/<uid>/<path:subpath>")
-def view(uid, subpath):
-    print(f"uid : {uid} / subpath : {subpath}")
-    return "Not Implemented"
+    try:
+        # Some website needs normal user-agent
+        res = r.get(url, headers={"User-Agent" : session['browser']})
+
+        if not (os.path.exists(f"{scraps_dir}/{session['id']}") and os.path.isdir(f"{scraps_dir}/{session['id']}")):
+            os.mkdir(f"{scraps_dir}/{session['id']}")
+
+        # download scrapped html source
+        fname = "scrap_" + random_string_generator(16)
+        f = open(f"{scraps_dir}/{session['id']}/{fname}", 'w')
+        f.write(res.text)
+        f.close()
+
+        scrap = Scrap(session["id"], fname, title)
+        db_session.add(scrap)
+        db_session.commit()
+
+        flash(f"Your scrap is stored successfully")
+        return redirect(url_for("index"))
+    except:
+        flash("Not a good url... hmm...")
+        return redirect(url_for("index"))
+
+@app.route("/view", methods=["GET"])
+def view():
+    if not "id" in session:
+        flash("You've not logged in")
+        return redirect(url_for("login"))
+
+    if not "name" in request.args:
+        return "name argument is required"
+
+    uid = session["id"]
+    fname = request.args["name"]
+    html = open(f"{scraps_dir}/{uid}/{fname}", "r").read()
+    res = make_response(html)
+    #res.headers['Content-Type'] = "text/html; charset=utf-8"
+    return res
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "GET":
         return render_template("register.html")
     elif request.method == "POST":
-        uid = request.form['id']
-        upw = request.form['pw']
+        uid = request.form.get("id", "")
+        upw = request.form.get("pw", "")
         if uid == "" or upw == "":
             flash("Not a valid id / pw")
-            return redirect(url_for("register"), code=302)
-
-        pwhash = generate_password_hash(upw)
+            return redirect(url_for("register"))
 
         if len(get_user(uid)) > 0:
             flash(f"User id {uid} already exists")
-            return redirect(url_for("register"), code=302)
+            return redirect(url_for("register"))
 
+        pwhash = generate_password_hash(upw)
         user = User(uid, pwhash)
         db_session.add(user)
         db_session.commit()
         flash("You were successfully registered")
-        return redirect(url_for("index"), code=302)
+        return redirect(url_for("index"))
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if "id" in session:
         flash("You've already logged in")
-        return redirect(url_for("index"), code=302)
+        return redirect(url_for("index"))
 
     if request.method == "GET":
         return render_template("login.html")
 
     elif request.method == "POST":
-        uid = request.form['id']
-        upw = request.form['pw']
+        uid = request.form.get("id", "")
+        upw = request.form.get("pw", "")
         if uid == "" or upw == "":
-            return "Not a valid id / pw"
-        
+            flash("Not a valid id / pw")
+            return redirect(url_for("index"))
+    
         users = get_user(uid, upw)
         if len(users) < 1:
             flash("No such user")
-            return redirect(url_for("login"), code=302)
+            return redirect(url_for("login"))
         if len(users) > 1:
             flash("It's weird... report to the admin")
-            return redirect(url_for("login"), code=302)
+            return redirect(url_for("login"))
 
         user = users[0]
         session['id'] = user.id
-        
+        session['browser'] = request.headers["User-Agent"]
+
         flash(f"Welcome {user.id}")
-        return redirect(url_for("index"), code=302)
+        return redirect(url_for("index"))
+
+@app.route("/mypage")
+def mypage():
+    if not "id" in session:
+        flash("You've not logged in yet")
+        return redirect(url_for("login"))
+
+    scraps = db_session.query(Scrap).filter(Scrap.owner_id==session['id']).all()
+    return render_template("mypage.html", scraps=scraps)
+
 
 @app.route("/logout")
 def logout():
     if not "id" in session:
         flash("You've not logged in yet")
-        return redirect(url_for("index"), code=302)
+        return redirect(url_for("index"))
 
     session.pop("id")
     flash("Bye")
-    return redirect(url_for("index"), code=302)
+    return redirect(url_for("index"))
 
-
-def argcheck():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-p", help="port of the server", type=int)
-    args = parser.parse_args()
-    return args
 
 if __name__ == "__main__":
     args = argcheck()
--- noob_scrapper/models.py
+++ noob_scrapper/models.py
@@ -13,3 +13,18 @@ class User(Base):
 
     def __repr__(self):
         return "<User('%d', '%s', '%s'>" %(self._id, self.id, self.pw)
+
+class Scrap(Base):
+    __tablename__ = "Scraps"
+    _id = Column(Integer, primary_key=True)
+    owner_id = Column(String(16))
+    name = Column(String)
+    title = Column(String)
+
+    def __init__(self, owner_id, name, title):
+        self.owner_id = owner_id
+        self.name = name
+        self.title = title
+
+    def __repr__(self):
+        return "<Scrap('%d', '%s', '%s', '%s'>" %(self._id, self.owner_id, self.name, self.title)
