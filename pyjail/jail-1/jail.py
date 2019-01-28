c91f892701af21023e014d01983b5b44853141e6
Dongpyeong Seo <bludrimjr@gmail.com>
2019-01-28 14:38:19 +0900

    pyjail: jail-1 added
    Call a function without using brackets!

---
+++ pyjail/jail-1/jail.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2
+
+class runner(object):
+    def __init__(self):
+        self.blacklist = []
+    def run_file(self, filename):
+        f = open(filename, 'r')
+        src = f.read()
+        self.run_code(src)
+        f.close()
+    def run_code(self, src):
+        if not self.is_safe(src):
+            print("Bad character was found! Go away!")
+            return
+        exec(src)
+    def is_safe(self, src):
+        if list(filter(lambda bad: bad in src, self.blacklist)):
+            return False
+        return True
+    def add_blacklist(self, blacklist):
+        if type(blacklist) == "list":
+            self.blacklist.extend(blacklist)
+        elif type(blacklist) == "str":
+            self.blacklist.extend(list(blacklist))
+
+
+r = runner()
+
+r.add_blacklist('()=')
+opt = raw_input("run file (f) / run code (c) : ")
+if opt == 'c':
+    r.run_code(raw_input("your src : "))
+elif opt == 'f':
+    r.run_file(raw_input("your filename : "))
